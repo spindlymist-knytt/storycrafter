e426b9b525363c3bc668a78801d4ef473d74ddf4
spindlymist <ocrobin@gmail.com>
2023-07-23 17:48:42 -0700

    convert Tilesets/Gradients tabs to new paradigm

--- EditorForm/TilesetsTab.cs
+++ EditorForm/TilesetsTab.cs
@@ -7,57 +7,59 @@ using System.Text;
 using System.Windows.Forms;
 
 namespace Story_Crafter {
-    partial class EditorForm {
-        class TilesetsTab {
-            EditorForm form;
-            ListViewItemComparer lvItemComparer = new ListViewItemComparer();
+    partial class TilesetsTab : UserControl, IEditorTab {
+        ListViewItemComparer lvItemComparer = new ListViewItemComparer();
 
-            public TilesetsTab(EditorForm parent) {
-                form = parent;
+        public TilesetsTab() {
+            this.InitializeComponent();
 
-                form.tileset_list.SelectedIndexChanged += delegate {
-                    if(form.tileset_list.SelectedItems.Count > 0) {
-                        Bitmap b = Program.LoadBitmap(Program.OpenStory.Path + @"\Tilesets\" + form.tileset_list.SelectedItems[0].Text);
-                        b.MakeTransparent(Color.Magenta);
-                        form.tileset_view.Image = b;
-                        form.tileset_view.Tag = Program.OpenStory.Path + @"\Tilesets\" + form.tileset_list.SelectedItems[0].Text;
-                        form.tileset_label.Text = form.tileset_list.SelectedItems[0].Text;
-                    }
-                };
-            }
-            public void StoryChanged() {
-                form.tileset_list.Clear();
-                DirectoryInfo dirInfo = new DirectoryInfo(Program.OpenStory.Path + @"\Tilesets");
-                if(!dirInfo.Exists) {
-                    form.tileset_label.Text = "";
-                    form.tileset_view.Image = null;
-                    return;
+            this.tileset_list.SelectedIndexChanged += delegate {
+                if(this.tileset_list.SelectedItems.Count > 0) {
+                    Bitmap b = Program.LoadBitmap(Program.OpenStory.Path + @"\Tilesets\" + this.tileset_list.SelectedItems[0].Text);
+                    b.MakeTransparent(Color.Magenta);
+                    this.tileset_view.Image = b;
+                    this.tileset_view.Tag = Program.OpenStory.Path + @"\Tilesets\" + this.tileset_list.SelectedItems[0].Text;
+                    this.tileset_label.Text = this.tileset_list.SelectedItems[0].Text;
                 }
-                FileInfo[] files = dirInfo.GetFiles("Tileset*.png");
-                if(files.Length > 0) {
-                    form.tileset_list.LargeImageList = new ImageList();
-                    form.tileset_list.LargeImageList.ImageSize = new Size(192, 101);
-                    form.tileset_list.LargeImageList.ColorDepth = ColorDepth.Depth24Bit;
-                    foreach(FileInfo f in files) {
-                        Bitmap b = (Bitmap)Image.FromFile(f.FullName);
-                        b.MakeTransparent(Color.Magenta);
-                        form.tileset_list.LargeImageList.Images.Add(b);
-                        form.tileset_list.Items.Add(f.Name).ImageIndex = form.tileset_list.LargeImageList.Images.Count - 1;
-                    }
-                    form.tileset_list.ListViewItemSorter = lvItemComparer;
-                    form.tileset_list.Sort();
-                    form.tileset_list.Items[0].Selected = true;
+            };
+        }
+
+        public void StoryChanged() {
+            this.tileset_list.Clear();
+            DirectoryInfo dirInfo = new DirectoryInfo(Program.OpenStory.Path + @"\Tilesets");
+            if(!dirInfo.Exists) {
+                this.tileset_label.Text = "";
+                this.tileset_view.Image = null;
+                return;
+            }
+            FileInfo[] files = dirInfo.GetFiles("Tileset*.png");
+            if(files.Length > 0) {
+                this.tileset_list.LargeImageList = new ImageList();
+                this.tileset_list.LargeImageList.ImageSize = new Size(192, 101);
+                this.tileset_list.LargeImageList.ColorDepth = ColorDepth.Depth24Bit;
+                foreach(FileInfo f in files) {
+                    Bitmap b = (Bitmap)Image.FromFile(f.FullName);
+                    b.MakeTransparent(Color.Magenta);
+                    this.tileset_list.LargeImageList.Images.Add(b);
+                    this.tileset_list.Items.Add(f.Name).ImageIndex = this.tileset_list.LargeImageList.Images.Count - 1;
                 }
-                else {
-                    form.tileset_label.Text = "";
-                    form.tileset_view.Image = null;
-                }
+                this.tileset_list.ListViewItemSorter = lvItemComparer;
+                this.tileset_list.Sort();
+                this.tileset_list.Items[0].Selected = true;
             }
-            public void TabOpened() {
+            else {
+                this.tileset_label.Text = "";
+                this.tileset_view.Image = null;
             }
-            public bool ProcessCmdKey(ref Message msg, Keys keyData) {
-                return false;
-            }
+        }
+
+        public void TabOpened() {
+        }
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData) {
+            return false;
+        }
+
+        public void ScreenChanged() {
         }
     }
 }
